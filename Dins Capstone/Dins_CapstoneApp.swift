710350aad10257f6933283fb94ce836023545a61
melissa-nguyen <n.melissa.25@gmail.com>
2021-08-09 11:04:03 -0700

    Initial Commit

---
+++ Dins Capstone/Dins_CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dins_CapstoneApp.swift
+//  Dins Capstone
+//
+//  Created by Melissa Nguyen on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Dins_CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
