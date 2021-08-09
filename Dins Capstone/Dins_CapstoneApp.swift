9e60f2ac607d789e598b62d922746aba938dc2c8
melissa-nguyen <n.melissa.25@gmail.com>
2021-08-09 11:08:53 -0700

    - Set up folders for views, models and view models - Renamed default content view to launch view

--- Dins Capstone/Dins_CapstoneApp.swift
+++ Dins Capstone/Dins_CapstoneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Dins_CapstoneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
--- Dins Capstone/Views/LaunchView.swift
+++ Dins Capstone/Views/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  Dins Capstone
 //
 //  Created by Melissa Nguyen on 8/9/21.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
